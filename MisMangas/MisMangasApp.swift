e00a915fc492c90481a11c3a44dea4b36bb26817
Guillermo Robinson <grobinson93@gmail.com>
2025-12-06 11:14:02 +0100

    Initial Commit

---
+++ MisMangas/MisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasApp.swift
+//  MisMangas
+//
+//  Created by Guillermo Robinson on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
