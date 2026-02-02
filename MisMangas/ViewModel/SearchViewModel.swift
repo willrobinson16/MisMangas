dcdfc6c1d24909be7b29c7a001f9f8d0b6643d48
Guillermo Robinson <grobinson93@gmail.com>
2026-02-02 21:33:07 +0100

    Actualización de SearchView, añadidos endpoints y métodos para search y cambios menores

---
+++ MisMangas/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  MisMangas
+//
+//  Created by Guillermo Robinson on 2/2/26.
+//
+
+import Foundation
---
+++ MisMangas/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  MisMangas
+//
+//  Created by Guillermo Robinson on 2/2/26.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
